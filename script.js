bc82e6d9c5fb21fc2917e7fad63d218a5539a245
Aktar Uz Zaman <{ID}+{username}@users.noreply.github.com>
2021-05-09 05:33:43 +0600

    HTML/CSS Part is done

---
+++ script.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationsButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const equalsButton = document.querySelectorAll('[data-equals]');
